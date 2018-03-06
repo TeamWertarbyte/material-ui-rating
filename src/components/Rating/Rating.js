d5317b9314dbee21050619f5476aa75e9c7c7844
David Moodie <davidmoodie12@gmail.com>
2018-03-06 17:18:15 +0000

    More Material UI 1 beta fixes and code changes

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -4,8 +4,13 @@ import { IconButton } from 'material-ui'
 import orange from 'material-ui/colors/orange'
 import grey from 'material-ui/colors/grey'
 import { Star, StarBorder } from 'material-ui-icons'
+import { withStyles } from 'material-ui/styles'
+import classNames from 'classnames';
 
 const styles = {
+  root: {},
+  iconButton: {},
+  icon: {},
   disabled: {
     pointerEvents: 'none'
   }
@@ -15,7 +20,7 @@ const styles = {
  * Material design star rating component for oyur star application!
  * @see [Card UI controls](https://material.io/guidelines/components/cards.html#cards-actions)
  */
-export default class Rating extends Component {
+class Rating extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -27,21 +32,42 @@ export default class Rating extends Component {
     const filled = i <= this.props.value
     const hovered = i <= this.state.hoverValue
 
+    const normalColor = this.props.disabled ? {} : {nativeColor: grey[300]};
+    const orangeColor = this.props.disabled ? {} : {nativeColor: orange[500]};
+
+    const iconHovered = !this.props.iconHovered ?
+      <StarBorder
+        classes={{ root: this.props.classes.icon }}
+        {...orangeColor} /> :
+      this.props.iconHovered
+
+    const iconFilled = !this.props.iconFilled ?
+      <Star
+        classes={{root: this.props.classes.icon}}
+        {...orangeColor} /> :
+      this.props.iconFilled
+
+    const iconNormal = !this.props.iconNormal ?
+      <StarBorder
+        className={this.props.classes.icon}
+        {...normalColor} /> :
+      this.props.iconNormal
+
     if ((hovered && !filled) || (!hovered && filled)) {
       return this.props.iconHoveredRenderer ? this.props.iconHoveredRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconHovered
+      }) : iconHovered
     } else if (filled) {
       return this.props.iconFilledRenderer ? this.props.iconFilledRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconFilled
+      }) : iconFilled
     } else {
       return this.props.iconNormalRenderer ? this.props.iconNormalRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconNormal
+      }) : iconNormal
     }
   }
 
@@ -49,12 +75,17 @@ export default class Rating extends Component {
     const rating = []
 
     for (let i = 1; i <= this.props.max; i++) {
-      const tooltip = this.props.tooltip || this.props.tooltipRenderer ? this.props.tooltipRenderer({index: i, ...this.props}) : null
+      // console.log("classes", this.props.classes)
 
       rating.push(
         <IconButton
           key={i}
-          classes={this.props.classes}
+          className={classNames(
+            this.props.classes.iconButton,
+            {
+              [this.props.classes.disabled]: this.props.disabled || this.props.readOnly
+            }
+          )}
           disabled={this.props.disabled}
           onMouseEnter={() => this.setState({hoverValue: i})}
           onMouseLeave={() => this.setState({hoverValue: this.props.value})}
@@ -69,21 +100,12 @@ export default class Rating extends Component {
       )
     }
 
-    return (
-      <div
-        style={this.props.disabled || this.props.readOnly ? {...styles.disabled, ...this.props.style} : this.props.style}
-      >
-        {rating}
-      </div>
-    )
+    return (<div>{rating}</div>)
   }
 }
 
 Rating.defaultProps = {
   disabled: false,
-  iconFilled: <Star nativeColor={orange[500]} />,
-  iconHovered: <StarBorder nativeColor={orange[500]} />,
-  iconNormal: <StarBorder nativeColor={grey[300]} />,
   tooltipPosition: 'bottom-center',
   max: 5,
   readOnly: false,
@@ -91,12 +113,10 @@ Rating.defaultProps = {
 }
 
 Rating.propTypes = {
-  /** Sets classname for IconButton component. */
-  className: PropTypes.string,
+  /** Sets classes for IconButton and SvgIcon components. */
+  classes: PropTypes.object.isRequired,
   /** Disables the rating and gray it out if set to true. */
   disabled: PropTypes.bool,
-  /** Sets classname for icon in IconButton Component. */
-  iconClassName: PropTypes.string,
   /** This is the icon to be used as an icon in value range. */
   iconFilled: PropTypes.node,
   /** Overrides filled icon renderer. */
@@ -109,26 +129,14 @@ Rating.propTypes = {
   iconNormal: PropTypes.node,
   /** Overrides normal icon renderer. */
   iconNormalRenderer: PropTypes.func,
-  /** Override the inline-icon-styles of the item elements. */
-  itemIconStyle: PropTypes.object,
-  /** Override the inline-styles of the item elements. */
-  itemStyle: PropTypes.object,
   /** The max value of the rating bar. */
   max: PropTypes.number,
   /** Fired when a value is clicked. */
   onChange: PropTypes.func,
   /** Don't allow input if set to true. */
   readOnly: PropTypes.bool,
-  /** Override the inline-styles of the root element. */
-  style: PropTypes.object,
-  /** Sets tooltip for icon in IconButton Component. */
-  tooltip: PropTypes.node,
-  /** Overrides tooltip renderer. */
-  tooltipRenderer: PropTypes.func,
-  /** Overrides tooltip position. */
-  tooltipPosition: PropTypes.string,
-  /** Overrides tooltip styles. */
-  tooltipStyles: PropTypes.object,
   /** The value of the rating bar. */
   value: PropTypes.number
 }
+
+export default withStyles(styles)(Rating);
