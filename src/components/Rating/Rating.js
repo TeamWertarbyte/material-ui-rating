ac83c91e876e5a4c80362df49cf4316422b0e29c
David Moodie <davidmoodie12@gmail.com>
2018-03-07 08:47:40 +0000

    Material UI v1 beta fixes and changes (#14)
    * Add package lock
    
    * Update material UI
    
    * Remove error generating props (will need to get functionality back later)
    
    * More Material UI 1 beta fixes and code changes
    
    * Remove commented code and add the root class to root div
    
    * Remove pointless onRate props from demos
    
    * Log out the rating clicked value in examples
    
    * Add empty test so Travis passes
    
    * Update some README documentation
    
    * Add StandardJS linter
    
    * Split disabled style into disabled and readOnly
    
    * Add CSS API documentation
    
    * Fix travis (use node stable version)

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -4,18 +4,26 @@ import { IconButton } from 'material-ui'
 import orange from 'material-ui/colors/orange'
 import grey from 'material-ui/colors/grey'
 import { Star, StarBorder } from 'material-ui-icons'
+import { withStyles } from 'material-ui/styles'
+import classNames from 'classnames'
+
+const noPointerEvents = {
+  pointerEvents: 'none'
+}
 
 const styles = {
-  disabled: {
-    pointerEvents: 'none'
-  }
+  root: {},
+  iconButton: {},
+  icon: {},
+  disabled: noPointerEvents,
+  readOnly: noPointerEvents
 }
 
 /**
  * Material design star rating component for oyur star application!
  * @see [Card UI controls](https://material.io/guidelines/components/cards.html#cards-actions)
  */
-export default class Rating extends Component {
+class Rating extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -27,46 +35,66 @@ export default class Rating extends Component {
     const filled = i <= this.props.value
     const hovered = i <= this.state.hoverValue
 
+    const normalColor = this.props.disabled ? {} : {nativeColor: grey[300]}
+    const orangeColor = this.props.disabled ? {} : {nativeColor: orange[500]}
+
+    const iconHovered = !this.props.iconHovered
+      ? <StarBorder
+        classes={{ root: this.props.classes.icon }}
+        {...orangeColor} />
+      : this.props.iconHovered
+
+    const iconFilled = !this.props.iconFilled
+      ? <Star
+        classes={{root: this.props.classes.icon}}
+        {...orangeColor} />
+      : this.props.iconFilled
+
+    const iconNormal = !this.props.iconNormal
+      ? <StarBorder
+        className={this.props.classes.icon}
+        {...normalColor} />
+      : this.props.iconNormal
+
     if ((hovered && !filled) || (!hovered && filled)) {
       return this.props.iconHoveredRenderer ? this.props.iconHoveredRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconHovered
+      }) : iconHovered
     } else if (filled) {
       return this.props.iconFilledRenderer ? this.props.iconFilledRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconFilled
+      }) : iconFilled
     } else {
       return this.props.iconNormalRenderer ? this.props.iconNormalRenderer({
         ...this.props,
         index: i
-      }) : this.props.iconNormal
+      }) : iconNormal
     }
   }
 
   render () {
+    const { classes, max, disabled, readOnly, value, onChange } = this.props
     const rating = []
 
-    for (let i = 1; i <= this.props.max; i++) {
-      const tooltip = this.props.tooltip || this.props.tooltipRenderer ? this.props.tooltipRenderer({index: i, ...this.props}) : null
-
+    for (let i = 1; i <= max; i++) {
       rating.push(
         <IconButton
           key={i}
-          className={this.props.className}
-          disabled={this.props.disabled}
-          iconStyle={this.props.itemIconStyle}
-          iconClassName={this.props.iconClassName}
-          style={this.props.itemStyle}
-          tooltip={tooltip}
-          tooltipPosition={this.props.tooltipPosition}
-          tooltipStyles={this.props.tooltipStyles}
+          className={classNames(
+            classes.iconButton,
+            {
+              [classes.disabled]: disabled,
+              [classes.readOnly]: readOnly
+            }
+          )}
+          disabled={disabled}
           onMouseEnter={() => this.setState({hoverValue: i})}
-          onMouseLeave={() => this.setState({hoverValue: this.props.value})}
-          onTouchTap={() => {
-            if (!this.props.readOnly && this.props.onChange) {
-              this.props.onChange(i)
+          onMouseLeave={() => this.setState({hoverValue: value})}
+          onClick={() => {
+            if (!readOnly && onChange) {
+              onChange(i)
             }
           }}
         >
@@ -75,34 +103,22 @@ export default class Rating extends Component {
       )
     }
 
-    return (
-      <div
-        style={this.props.disabled || this.props.readOnly ? {...styles.disabled, ...this.props.style} : this.props.style}
-      >
-        {rating}
-      </div>
-    )
+    return (<div className={classes.root}>{rating}</div>)
   }
 }
 
 Rating.defaultProps = {
   disabled: false,
-  iconFilled: <Star color={orange[500]} />,
-  iconHovered: <StarBorder color={orange[500]} />,
-  iconNormal: <StarBorder color={grey[300]} />,
-  tooltipPosition: 'bottom-center',
   max: 5,
   readOnly: false,
   value: 0
 }
 
 Rating.propTypes = {
-  /** Sets classname for IconButton component. */
-  className: PropTypes.string,
+  /** Useful to extend the style applied to components. See the repository README for the accepted keys. */
+  classes: PropTypes.object.isRequired,
   /** Disables the rating and gray it out if set to true. */
   disabled: PropTypes.bool,
-  /** Sets classname for icon in IconButton Component. */
-  iconClassName: PropTypes.string,
   /** This is the icon to be used as an icon in value range. */
   iconFilled: PropTypes.node,
   /** Overrides filled icon renderer. */
@@ -115,26 +131,14 @@ Rating.propTypes = {
   iconNormal: PropTypes.node,
   /** Overrides normal icon renderer. */
   iconNormalRenderer: PropTypes.func,
-  /** Override the inline-icon-styles of the item elements. */
-  itemIconStyle: PropTypes.object,
-  /** Override the inline-styles of the item elements. */
-  itemStyle: PropTypes.object,
   /** The max value of the rating bar. */
   max: PropTypes.number,
   /** Fired when a value is clicked. */
   onChange: PropTypes.func,
   /** Don't allow input if set to true. */
   readOnly: PropTypes.bool,
-  /** Override the inline-styles of the root element. */
-  style: PropTypes.object,
-  /** Sets tooltip for icon in IconButton Component. */
-  tooltip: PropTypes.node,
-  /** Overrides tooltip renderer. */
-  tooltipRenderer: PropTypes.func,
-  /** Overrides tooltip position. */
-  tooltipPosition: PropTypes.string,
-  /** Overrides tooltip styles. */
-  tooltipStyles: PropTypes.object,
   /** The value of the rating bar. */
   value: PropTypes.number
 }
+
+export default withStyles(styles)(Rating)
--- src/components/Rating/Rating.md
+++ src/components/Rating/Rating.md
@@ -1,118 +1,117 @@
 Normal:
 ```
     <Rating
-      onRate={() => console.log('onRate')}
       value={3}
       max={5}
-      onChange={() => console.log('onChange')}
+      onChange={(i) => console.log('onChange ' + i)}
     />
 ```
 Read Only:
 ```
        <Rating
-          onRate={() => console.log('onRate')}
           value={3}
           max={5}
-          onChange={() => console.log('onChange')}
+          onChange={(i) => console.log('onChange ' + i)}
           readOnly
         />
 ```
 Disabled:
 ```
        <Rating
-          onRate={() => console.log('onRate')}
           value={3}
           max={5}
-          onChange={() => console.log('onChange')}
+          onChange={(i) => console.log('onChange ' + i)}
           disabled
         />
 ```
 Custom Icons:
 ```
 const { AddCircle, AddCircleOutline, Remove } = require('material-ui-icons');
-const green = require('material-ui/colors/green');
-const red = require('material-ui/colors/red');
+const green = require('material-ui/colors/green').default;
+const red = require('material-ui/colors/red').default;
     <Rating
-      onRate={() => console.log('onRate')}
       value={3}
       max={5}
-      onChange={() => console.log('onChange')}
-      iconFilled={<AddCircle color={green[500]} />}
-      iconHovered={<AddCircleOutline color={green[500]} />}
-      iconNormal={<Remove color={red[300]} />}
+      onChange={(i) => console.log('onChange ' + i)}
+      iconFilled={<AddCircle nativeColor={green[500]} />}
+      iconHovered={<AddCircleOutline nativeColor={green[500]} />}
+      iconNormal={<Remove nativeColor={red[300]} />}
     />
 ```
 Custom Sizes:
 ```
-const styles = {
-  smallIcon: {
-    width: 36,
-    height: 36
-  },
-  mediumIcon: {
-    width: 48,
-    height: 48
-  },
-  largeIcon: {
-    width: 60,
-    height: 60
-  },
-  small: {
+const { withStyles } = require('material-ui/styles');
+const smallStyles = {
+  iconButton: {
     width: 72,
     height: 72,
     padding: 16
   },
-  medium: {
+  icon: {
+    width: 36,
+    height: 36
+  }
+};
+const mediumStyles = {
+  iconButton: {
     width: 96,
     height: 96,
     padding: 24
   },
-  large: {
+  icon: {
+    width: 48,
+    height: 48
+  }
+};
+const largeStyles = {
+  iconButton: {
     width: 120,
     height: 120,
     padding: 30
+  },
+  icon: {
+    width: 60,
+    height: 60
   }
 };
+const MyRating = ({classes}) => (
+  <Rating
+    value={3}
+    max={5}
+    onChange={(i) => console.log('onChange ' + i)}
+    classes={classes}
+  />
+);
+const SmallRating = withStyles(smallStyles)(MyRating);
+const MediumRating = withStyles(mediumStyles)(MyRating);
+const LargeRating = withStyles(largeStyles)(MyRating);
     <div>
-      <Rating
-        onRate={() => console.log('onRate')}
-        value={3}
-        max={5}
-        onChange={() => console.log('onChange')}
-        itemStyle={styles.small}
-        itemIconStyle={styles.smallIcon}
-      />
-      <Rating
-        onRate={() => console.log('onRate')}
-        value={3}
-        max={5}
-        onChange={() => console.log('onChange')}
-        itemStyle={styles.medium}
-        itemIconStyle={styles.mediumIcon}
-      />
-      <Rating
-        onRate={() => console.log('onRate')}
-        value={3}
-        max={5}
-        onChange={() => console.log('onChange')}
-        itemStyle={styles.large}
-        itemIconStyle={styles.largeIcon}
-      />
+      <SmallRating />
+      <MediumRating />
+      <LargeRating />
     </div>
 ```
 Dynamic:
 ```
 const { AddCircle, AddCircleOutline, Remove } = require('material-ui-icons');
-const green = require('material-ui/colors/green');
-      <Rating
-        onRate={() => console.log('onRate')}
-        value={3}
-        max={5}
-        onChange={() => console.log('onChange')}
-        itemIconStyle={{color : green[300]}}
-        iconFilledRenderer={({index}) => <AddCircle color={green[500]} />}
-        iconNormalRenderer={({index}) => <AddCircleOutline color={green[500]} />}
-        tooltipRenderer={({index}) => <span>{index}</span>}
-        tooltipPosition='bottom-center'
-      />
+const { withStyles } = require('material-ui/styles');
+const green = require('material-ui/colors/green').default;
+const styles = {
+  icon: {
+    color: green[300]
+  }
+};
+const MyRating = ({classes}) => (
+  <Rating
+    value={3}
+    max={5}
+    onChange={(i) => console.log('onChange ' + i)}
+    iconFilledRenderer={({index}) => <AddCircle nativeColor={green[500]} />}
+    iconHoveredRenderer={({index}) => <span className={classes.icon}>{index}</span>}
+    iconNormalRenderer={({index}) => <AddCircleOutline nativeColor={green[500]} />}
+    classes={classes}
+  />
+);
+const MyRatingWithStyles = withStyles(styles)(MyRating);
+    <MyRatingWithStyles/>
 ```
