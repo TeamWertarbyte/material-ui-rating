4fdbe30fb2973c333f79a10c933d9bb75bc5887b
leMaik <an@maik.tk>
2019-02-12 22:33:14 +0100

    Add a storybook for examples.

---
+++ stories/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Rating from '../src'
+
+storiesOf('Rating', module)
+  .add('with some chips', () => (
+    <Rating
+      value={3}
+      max={5}
+      onChange={action('onChange')}
+    />
+  ))
+  .add('read only', () => (
+    <Rating
+      value={3}
+      max={5}
+      readOnly
+    />
+  ))
+  .add('disabled', () => (
+    <Rating
+      value={3}
+      max={5}
+      disabled
+    />
+  ))
+  .add('with custom icons', () => {
+    const { AddCircle, AddCircleOutline, Remove } = require('@material-ui/icons')
+    const green = require('@material-ui/core/colors/green').default
+    const red = require('@material-ui/core/colors/red').default
+
+    return (
+      <Rating
+        value={3}
+        max={5}
+        onChange={(i) => console.log('onChange ' + i)}
+        iconFilled={<AddCircle nativeColor={green[500]} />}
+        iconHovered={<AddCircleOutline nativeColor={green[500]} />}
+        iconNormal={<Remove nativeColor={red[300]} />}
+      />
+    )
+  })
